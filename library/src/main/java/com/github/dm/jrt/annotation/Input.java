0102f526708c9d584bbf8d3aae71eb9c5cc5d7af
Davide Maestroni <davide.maestroni@gmail.com>
2015-09-27 20:59:16 +0200

    Cosmetics

--- library/src/main/java/com/github/dm/jrt/annotation/Input.java
+++ library/src/main/java/com/github/dm/jrt/annotation/Input.java
@@ -101,6 +101,14 @@ public @interface Input {
          */
         VALUE,
         /**
+         * Element mode.<br/>
+         * Each element of the input collection or array is passed separately to the wrapped method.
+         * <p/>
+         * The annotated parameter must be an array or implement an {@link java.lang.Iterable} and
+         * must be the only parameter accepted by the method.
+         */
+        ELEMENT,
+        /**
          * Collection mode.<br/>
          * The inputs are collected from the channel and passed as an array or collection to the
          * wrapped method.
@@ -108,14 +116,6 @@ public @interface Input {
          * The annotated parameter must extend an {@link com.github.dm.jrt.channel.OutputChannel
          * OutputChannel} and must be the only parameter accepted by the method.
          */
-        COLLECTION,
-        /**
-         * Element mode.<br/>
-         * Each element of the input collection or array is passed separately to the wrapped method.
-         * <p/>
-         * The annotated parameter must be an array or implement an {@link java.lang.Iterable} and
-         * must be the only parameter accepted by the method.
-         */
-        ELEMENT
+        COLLECTION
     }
 }
